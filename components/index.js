260ab44de81abc11e52eb39b58499f7caf48a256
rohit <rhtry.tech@gmail.com>
2022-12-26 13:56:33 +0530

    adding files ...

---
+++ components/index.js
@@ -0,0 +1,9 @@
+export {default as PostCard} from "./PostCard.jsx";
+
+export {default as Categories} from "./Categories.jsx";
+
+export {default as PostWidget} from "./PostWidget.jsx";
+
+export {default as Header} from "./Header.jsx";
+
+export {default as Layout} from "./Layout.jsx";
