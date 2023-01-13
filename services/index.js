2df91d3b6f4509ce81ca6fd18505e6121758694c
rohit <rhtry.tech@gmail.com>
2023-01-13 12:30:53 +0530

    updating files ...!

--- services/index.js
+++ services/index.js
@@ -3,6 +3,8 @@
 import {request, gql} from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+// functionForFetchingPosts
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
@@ -36,4 +38,48 @@ export const getPosts = async () => {
 
   const result = await request(graphqlAPI, query);
   return result.postsConnection.edges;
+};
+
+// functionForFetchingRecentPosts
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+// functionForFetchingSimilarPosts
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, {slug, categories});
+
+  return result.posts;
 };
